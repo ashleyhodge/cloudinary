9ac26cfeab9342858b15f8de43c856dcab867053
ashleyhodge <ashhodge@comcast.net>
2023-01-19 14:25:16 -0500

    send to cloudinary

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express();
+const { cloudinary } = require('./utils/cloudinary');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.post('/api/upload', (req, res) => {
+app.get('/api/images', async (req, res) => {
+  const {resources} = await cloudinary.search
+  .expression('folder:dev_setups')
+  .sort_by('public_id', 'desc')
+  .max_results(30)
+  .execute();
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+})
+
+app.post('/api/upload', async (req, res) => {
   try {
     const fileStr = req.body.data;
-    console.log(fileStr);
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'dev_setups'
+    });
+    console.log(uploadedResponse);
+    res.json({msg: 'YAYAYA'})
   } catch (error) {
     console.log(error)
+    res.status(500).json({err: 'Something went wrong'})
   }
 })
 
